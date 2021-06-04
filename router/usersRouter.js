123c645ff639901d621c0e2225bcd8be940e2d16
ferdous85 <ferdous85@gmail.com>
2021-06-04 06:25:43 +0600

    user module

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -1,12 +1,17 @@
 //external imports
 const router = require('express').Router()
 
+
 //internal imports
 const { getUsers } = require('../controller/usersController')
 const decorateHtmlResponse = require('../middlewares/common/decorateHtmlResponse')
+const avatarUpload = require('../middlewares/users/avatarUpload')
+const { addUserValidators, addUserValidationHandler } = require('../middlewares/users/userValidators')
 
 
-// login page
+// users page
 router.get('/',decorateHtmlResponse('Users'), getUsers)
+
+router.post('/', avatarUpload, addUserValidators, addUserValidationHandler)
 
 module.exports = router
